0a69e8237e37aa0a6ff3d2fd722f61a2ab3651b6
Plus1XP <evldev@protonmail.ch>
2021-07-06 10:21:39 +0100

    Test: Add Between Tests

---
+++ EVLlib/EVLlib.UnitTests/Extentensions/StringExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EVLlib.Extentensions;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVLlib.Extentensions.Tests
+{
+    [TestClass()]
+    public class StringExtensionTests
+    {
+        string normalTestString = "Hello! i am a simple string for testing purposes. " +
+                                    "Please be gentle!";
+
+        string multipleSpacedTestString = "   Hello! i     am a simple string     with far too many  " +
+                "spaces...  Please    Help!   ";
+
+        [TestMethod()]
+        public void BetweenTest()
+        {
+            string expected = "for testing purposes. ";
+
+            string results = normalTestString.Between("string ", "Please");
+
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
