04cbfa7579ce25af10ee8b54df3d2671a0f1c5c9
Plus1XP <evldev@protonmail.ch>
2021-07-15 22:27:56 +0100

    Feat: Add ClearScreen Method to TextUI Class

---
+++ EVLlib/EVLlib/ConsoleTools/TextUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EVLlib.ConsoleTools
+{
+    class TextUI
+    {
+        /// <summary>
+        /// Clears console.
+        /// </summary>
+        public void ClearScreen()
+        {
+            Console.Clear();
+        }
+    }
+}
