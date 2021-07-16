1e6c477f37aa981a9ba07b216869d55f6b1be232
Plus1XP <evldev@protonmail.ch>
2021-07-16 21:31:24 +0100

    Feat: Add PrintBlankLine Method to TextUI Class

--- EVLlib/EVLlib/ConsoleTools/TextUI.cs
+++ EVLlib/EVLlib/ConsoleTools/TextUI.cs
@@ -8,6 +8,17 @@ namespace EVLlib.ConsoleTools
     class TextUI
     {
         /// <summary>
+        /// Gets the newline string.
+        /// </summary>
+        /// <remarks>
+        /// Uses Environment.NewLine (\r\n).
+        /// </remarks>
+        public void PrintBlankLine()
+        {
+            Console.Write(Environment.NewLine);
+        }
+
+        /// <summary>
         /// Clears console.
         /// </summary>
         public void ClearScreen()
