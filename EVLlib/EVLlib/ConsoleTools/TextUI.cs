45df7b5b4be15ccdf4d376f8b5e2752a6e4a3c4d
Plus1XP <evldev@protonmail.ch>
2021-07-24 16:37:53 +0100

    Feat: Add GetNumericResponse Method to TextUI Class

--- EVLlib/EVLlib/ConsoleTools/TextUI.cs
+++ EVLlib/EVLlib/ConsoleTools/TextUI.cs
@@ -8,6 +8,29 @@ namespace EVLlib.ConsoleTools
     class TextUI
     {
         /// <summary>
+        /// Reads a String (numbers) from the console using Console.ReadLine.
+        /// </summary>
+        /// <remarks>
+        /// Method will loop until a valid number is entered.
+        /// (Unacceptable values include non-numeric or Values outside of the Min/Max Scope).
+        /// </remarks>
+        /// <param name="DisplayText">String to be displayed before user input.</param>
+        /// <param name="minSelection">The minimum Int to be accepted</param>
+        /// <param name="maxSelection">the Maximum Int to be accepted</param>
+        /// <returns>A Int</returns>
+        public int GetNumericResponse(string DisplayText, int minSelection, int maxSelection)
+        {
+            int selection;
+            do
+            {
+                this.PrintBlankLine();
+                this.Print(DisplayText);
+            } while (!int.TryParse(this.GetResponse(), out selection) || !(selection >= minSelection && selection <= maxSelection));
+
+            return selection;
+        }
+
+        /// <summary>
         /// Executes Console.ReadLine Method.
         /// </summary>
         /// <returns>String of characters from input stream.</returns>
