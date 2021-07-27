e3a846883aa599e08a880740a6e486a17d08cac8
Plus1XP <evldev@protonmail.ch>
2021-07-27 09:20:17 +0100

    Feat: Add Constructor to TextHeader Class

---
+++ EVLlib/EVLlib/ConsoleTools/TextHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace EVLlib.ConsoleTools
+{
+    class TextHeader : TextUI
+    {
+        private string title;
+        private string version;
+        private string release;
+        private string contact;
+        private string border;
+        private string dismiss;
+
+        public TextHeader()
+        {
+            this.title = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductName;
+            this.version = $"Version {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion}";
+            this.release = File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location).ToString("dd MMMM yyyy");
+            this.contact = "http://www.github.com/Plus1XP";
+            this.border = $"+ {new string('-', Console.WindowWidth / 2)} +";
+            this.dismiss = "Press any key to dismiss...";
+        }
+    }
+}
