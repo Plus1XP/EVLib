affedaef345121fee1373e60f654705ba399f6ad
Plus1XP <evldev@protonmail.ch>
2021-08-08 11:18:50 +0100

    Add IsFolderCreated Method to FileManager Class

---
+++ EVLlib/EVLlib/FileIO/FileManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EVLlib.FileIO
+{
+    public class FileManager
+    {
+        /// <summary>
+        /// Checks if folder exists.
+        /// </summary>
+        /// <param name="filePath">The file to write to.</param>
+        /// <returns>Boolean true if folder exists, false if folder does not exist.</returns>
+        public bool IsFolderCreated(string filePath)
+        {
+            return Directory.Exists(filePath);
+        }
+    }
+}
