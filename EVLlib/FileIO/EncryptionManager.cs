34cca6c53c160df2e864e21e79d30e96dfd20d86
Plus1XP <evldev@protonmail.ch>
2021-09-03 15:15:04 +0100

    Add EncryptionManager Class

---
+++ EVLlib/FileIO/EncryptionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EVLlib.FileIO
+{
+    public class EncryptionManager
+    {
+        Aes aes;
+        private byte[] key;
+        private byte[] iv;
+
+        /// <summary>
+        /// Create a new instance of the default AES implementation class
+        /// </summary>
+        public EncryptionManager()
+        {
+            aes = Aes.Create();
+        }
+    }
+}
