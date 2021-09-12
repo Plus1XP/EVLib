5eead6ed8b5fc7955c787553df6bdc5d6e710ef6
Plus1XP <evldev@protonmail.ch>
2021-09-12 17:44:10 +0100

    Add CalendarManager Class

---
+++ EVLlib/FileIO/CalendarManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVLlib.FileIO
+{
+    public class CalendarManager : FileManager
+    {
+        private StringBuilder calendarEntry;
+
+        public CalendarManager()
+        {
+
+        }
+    }
+}
