7948fd985b85f85f05e7537f7ef7f2643749c042
Plus1XP <evldev@protonmail.ch>
2021-11-08 20:26:05 +0000

    Add EncryptorManagerTest Class

---
+++ EVLlib.UnitTests/FileIO/EncryptorManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EVLlib.FileIO.Tests
+{
+    [TestClass]
+    public class EncryptorManagerTests
+    {
+        private string encryptionKey = "7pGG9!ech449*10";
+        private string sampleText = "Hi, I am sample text to be used in this test. Can you see me? o_O";
+
+        private const string folderName = "PleaseDelete";
+        private const string fileName = "PleaseDelete.txt";
+        private string testDirectory = $"{Directory.GetCurrentDirectory()}\\{folderName}";
+        private string testFile = $"{Directory.GetCurrentDirectory()}\\{folderName}\\{fileName}";
+    }
+}
