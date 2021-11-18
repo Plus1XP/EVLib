487a460e805d65b48cf6b13c067d058fbca6edd6
Plus1XP <evldev@protonmail.ch>
2021-11-18 21:56:52 +0000

    Add MessageField Class

---
+++ EVLlib/Mail/MessageField.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVLlib.Mail
+{
+    public class MailParameters
+    {
+        public string SenderName;
+        public string SenderEmail;
+        public string RecipientName;
+        public string RecipientEmail;
+        public string Subject;
+        public string Body;
+        public string AttachmentPath = null;
+
+        public MailParameters()
+        {
+
+        }
+
+        public MailParameters(string senderName, string senderEmail, string recipientName,
+            string recipientEmail, string subject, string body, string attachmentPath = null)
+        {
+            SenderName = senderName;
+            SenderEmail = senderEmail;
+            RecipientName = recipientName;
+            RecipientEmail = recipientEmail;
+            Subject = subject;
+            Body = body;
+            AttachmentPath = attachmentPath;
+        }
+    }
+}
