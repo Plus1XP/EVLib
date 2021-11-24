394a65e8839011caaae18d1a16adeb141ba7cb48
Plus1XP <evldev@protonmail.ch>
2021-11-24 14:36:13 +0000

    Add MailTest Class

---
+++ EVLlib.UnitTests/Mail/MailTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EVLlib.Mail.Tests
+{
+    [TestClass]
+    public class ClientTest
+    {
+        ServerSettings serverSettings = new ServerSettings()
+        {
+            Host = "smtp.host.com",
+            Port = 587,
+            Username = "username@host.com",
+            Password = "StrongPassword1",
+            EnableSsl = true
+        };
+
+        MessageField messageField = new MessageField()
+        {
+            SenderName = "Sender",
+            SenderEmail = "sender@host.com",
+            RecipientName = "Recipient",
+            RecipientEmail = "recipient@host.com",
+            Subject = "Test Message",
+            Body = "Testing mail-flow"
+        };
+
+}
